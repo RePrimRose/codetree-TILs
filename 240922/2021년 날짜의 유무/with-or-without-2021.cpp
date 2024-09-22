552204d0b61b7cb308bdd61dbf07292e4f2e3b78
RePrimRose <park1088230@gmail.com>
2024-09-22 21:51:25 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240922/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isVaildDate(int m, int d) {
+    if(m > 12) return false;
+    if(d > 31) return false;
+    if(m == 2 || d > 28) return false;
+    if(m == 4 || m == 6 || m == 9 || m == 11) {
+        if(d > 30) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m, d;
+
+    cin >> m >> d;
+
+    if(isVaildDate(m, d)) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
