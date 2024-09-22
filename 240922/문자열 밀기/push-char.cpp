bf03a09bc78bb55e6c4d87f0b8284e46a0fdf824
RePrimRose <park1088230@gmail.com>
2024-09-22 15:14:20 +0900

    Title: 문자열 밀기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/문자열 밀기/push-char.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+
+    cin >> str;
+
+    str = str.substr(1, str.length() - 1) + str.substr(0, 1);
+
+    cout << str;
+
+    return 0;
+}
