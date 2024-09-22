921b087829707b3c8fe18e7cfbc81eb22ea68567
RePrimRose <park1088230@gmail.com>
2024-09-22 16:16:25 +0900

    Title: 대문자 소문자 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/대문자 소문자 바꾸기/change-uppercase-and-lowercase.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+
+    cin >> str;
+
+    for(int i = 0; i < str.length(); i++) {
+        char c = str[i];
+
+        if(c >= 'A' && c <= 'Z') cout << (char)tolower(c);
+        else cout << (char)toupper(c);
+    }
+
+    return 0;
+}
