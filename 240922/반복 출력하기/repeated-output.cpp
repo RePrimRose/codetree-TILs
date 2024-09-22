fb5e32fc48be981fad3c7b3c70b4e2bd8256cb10
RePrimRose <park1088230@gmail.com>
2024-09-22 18:23:21 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printLine(int n) {
+    for(int i = 0; i < n; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    cin >> n;
+
+    printLine(n);
+
+    return 0;
+}
