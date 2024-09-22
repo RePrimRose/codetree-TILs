c06ff23c931ccc3f3063192ff924b7862c0b8894
RePrimRose <park1088230@gmail.com>
2024-09-22 22:03:11 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240922/그 계절, 그 날/that-season-that-day.cpp
+++ 240922/그 계절, 그 날/that-season-that-day.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 bool isLeapYear(int y) {
     if(y % 4 != 0) return false;
-    if(y % 100 == 0 && y % 400 != 0) return false;
+    if(y % 100 == 0 && y % 400 == 0) return true;
+    if(y % 100 == 0) return false;
 
     return true;
 }
