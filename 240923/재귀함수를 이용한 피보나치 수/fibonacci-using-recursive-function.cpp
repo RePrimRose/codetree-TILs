0f4a74ef6d1cee6e81387216c589d886aa5630b7
RePrimRose <park1088230@gmail.com>
2024-09-23 14:41:58 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n) {
+    if(n < 3) return 1;
+
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    cin >> n;
+
+    cout << fibo(n);
+
+    return 0;
+}
