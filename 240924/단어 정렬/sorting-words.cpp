350887d5a58cd5a9f9d9418e321b8bd925976bfa
RePrimRose <park1088230@gmail.com>
2024-09-24 12:10:32 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240924/단어 정렬/sorting-words.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    string arr[100];
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
