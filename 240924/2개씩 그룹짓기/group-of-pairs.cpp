9cf85db4c956292f10df6daad6aefa12fdf2a72e
RePrimRose <park1088230@gmail.com>
2024-09-24 12:18:05 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240924/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int arr[2000];
+
+    cin >> n;
+
+    for(int i = 0; i < 2 * n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + 2 * n);
+
+    int max = 0;
+    for(int i = 0; i < n; i++) {
+        max = (max, arr[i] + arr[2 * n - (i + 1)]);
+    }
+
+    cout << max;
+
+    return 0;
+}
