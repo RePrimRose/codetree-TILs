1c4fe80d74c5fdfa33a71f32aa9e30b9a0ac28b2
RePrimRose <park1088230@gmail.com>
2024-09-29 18:07:19 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240929/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int numsOfDays[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int getDays(int m, int d) {
+    int days = 0;
+
+    for(int i = 1; i < m; i++) {
+        days += numsOfDays[i];
+    }
+
+    return days + d;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m1, d1, m2, d2, cnt;
+    string weekName;
+
+    cin >> m1 >> d1 >> m2 >> d2 >> weekName;
+
+    int m1Days = getDays(m1, d1);
+    int m2Days = getDays(m2, d2);
+
+    if(weekName == "Tue") {
+        m1Days += 1;
+    } else if(weekName == "Wed") {
+        m1Days += 2;
+    } else if(weekName == "Thu") {
+        m1Days += 3;
+    } else if(weekName == "Fri") {
+        m1Days += 4;
+    } else if(weekName == "Sat") {
+        m1Days += 5;
+    } else if(weekName == "Sun") {
+        m1Days += 6;
+    }
+
+    cnt = (m2Days - m1Days) / 7 + 1;
+
+    cout << cnt;
+
+    return 0;
+}
