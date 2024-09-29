6c570636cb61fa098cba8a411dbf39a67c2b5629
RePrimRose <park1088230@gmail.com>
2024-09-29 17:27:35 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240929/Date to Date/date-to-date.cpp
+++ 240929/Date to Date/date-to-date.cpp
@@ -6,14 +6,22 @@ int main() {
     int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int m1, d1, m2, d2;
     int elapsed_days = 0;
+    int m1_days = 0;
+    int m2_days = 0;
 
     cin >> m1 >> d1 >> m2 >> d2;
 
-    elapsed_days = num_of_days[m1] - d1 + 1;
-    for(int i = m1 + 1; i < m2; i++) {
-        elapsed_days += num_of_days[i];
+    for(int i = 1; i < m1; i++) {
+        m1_days += num_of_days[i];
     }
-    elapsed_days += d2;
+    m1_days += d1;
+
+    for(int i = 1; i < m2; i++) {
+        m2_days += num_of_days[i];
+    }
+    m2_days += d2;
+
+    elapsed_days = m2_days - m1_days + 1;
 
     cout << elapsed_days;
 
