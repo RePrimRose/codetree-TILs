f2ae6a72e9eb2b17919313d4d4d9c0d6fa717f61
RePrimRose <park1088230@gmail.com>
2024-10-06 20:23:45 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241006/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int ans[100][100];
+int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    int x = n / 2, y = n / 2, dir = 0;
+    ans[x][y] = 1;
+
+    for(int i = 2; i <= n * n; i++) {
+        int nx = x + dx[dir], ny = y + dy[dir];
+
+        if(!InRange(nx, ny) || ans[nx][ny] != 0) dir = (dir - 1 + 4) % 4;
+
+        x = x + dx[dir]; y = y + dy[dir];
+        ans[x][y] = i;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
