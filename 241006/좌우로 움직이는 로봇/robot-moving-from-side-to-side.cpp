b419bebb2822bf3e820be795fd4e3bd74e42d49a
RePrimRose <park1088230@gmail.com>
2024-10-06 14:01:44 +0900

    Title: 좌우로 움직이는 로봇, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241006/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_RANGE 1000001
+
+int a[MAX_RANGE];
+int b[MAX_RANGE];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+
+    int timeA = 1;
+    for(int i = 0; i < n; i++) {
+        int t;
+        char d;
+        cin >> t >> d;
+
+        while(t--) {
+            if(d == 'R') {
+                a[timeA] = a[timeA - 1] + 1;
+            } else {
+                a[timeA] = a[timeA - 1] - 1;
+            }
+            timeA++;
+        }
+    }
+
+    int timeB = 1;
+    for(int i = 0; i < m; i++) {
+        int t;
+        char d;
+        cin >> t >> d;
+
+        while(t--) {
+            if(d == 'R') {
+                b[timeB] = b[timeB - 1] + 1;
+            } else {
+                b[timeB] = b[timeB - 1] - 1;
+            }
+            timeB++;
+        }
+    }
+
+    int ans = 0;
+    for(int i = 1; i < MAX_RANGE; i++) {
+        if(a[i] == b[i] && a[i - 1] != b[i - 1]) ans++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
