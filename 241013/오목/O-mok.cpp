46863b83f81be5984d14773a2df5e20d552c690f
RePrimRose <park1088230@gmail.com>
2024-10-13 17:08:41 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241013/오목/O-mok.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#define N 19
+
+int arr[N][N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int ans = 0, x = 0, y = 0;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            int curr = arr[i][j];
+            if(curr == 1 || curr == 2) {
+                int rightStone = 1, underStone = 1, diagonalStone = 1;
+                for(int k = 1; k < 6; k++) {
+                    if(arr[i][j + k * 1] == curr && j + k * 1 < N) rightStone++;
+                    if(arr[i + k * 1][j] == curr && i + k * 1 < N) underStone++;
+                    if(arr[i + k * 1][j + k * 1] == curr && j + k * 1 < N && i + k * 1 < N) diagonalStone++;
+                }
+                if(rightStone == 5) {
+                    ans = curr;
+                    x = j + 1;
+                    y = i + 1;
+                }
+                if(underStone == 5) {
+                    ans = curr;
+                    x = j + 1;
+                    y = i + 1;
+                }
+                if(diagonalStone == 5) {
+                    ans = curr;
+                    x = j - 1;
+                    y = i - 1;
+                }
+            }
+        }
+    }
+
+    cout << ans << endl;
+    cout << y << " " << x;
+
+    return 0;
+}
