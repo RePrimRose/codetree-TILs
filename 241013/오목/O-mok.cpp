ce0faa7335dcf8ab0e5b2100e4f5632a702177f2
RePrimRose <park1088230@gmail.com>
2024-10-13 17:27:20 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241013/오목/O-mok.cpp
+++ 241013/오목/O-mok.cpp
@@ -18,11 +18,12 @@ int main() {
         for(int j = 0; j < N; j++) {
             int curr = arr[i][j];
             if(curr == 1 || curr == 2) {
-                int rightStone = 1, underStone = 1, diagonalStone = 1;
+                int rightStone = 1, underStone = 1, diagonalDownStone = 1, diagonalUpStone = 1;
                 for(int k = 1; k < 5; k++) {
-                    if(arr[i][j + k * 1] == curr && j + k * 1 < N) rightStone++;
-                    if(arr[i + k * 1][j] == curr && i + k * 1 < N) underStone++;
-                    if(arr[i + k * 1][j + k * 1] == curr && j + k * 1 < N && i + k * 1 < N) diagonalStone++;
+                    if(j + k * 1 < N && arr[i][j + k * 1] == curr) rightStone++;
+                    if(i + k * 1 < N && arr[i + k * 1][j] == curr) underStone++;
+                    if(j + k * 1 < N && i + k * 1 < N && arr[i + k * 1][j + k * 1] == curr) diagonalDownStone++;
+                    if(j + k * 1 < N && i - k * 1 >= 0 && arr[i - k * 1][j + k * 1] == curr) diagonalUpStone++;
                 }
                 if(rightStone == 5) {
                     ans = curr;
@@ -34,10 +35,15 @@ int main() {
                     x = j + 1;
                     y = i + 3;
                 }
-                if(diagonalStone == 5) {
+                if(diagonalDownStone == 5) {
                     ans = curr;
                     x = j + 3;
                     y = i + 3;
+                }
+                if(diagonalUpStone == 5) {
+                    ans = curr;
+                    x = j + 3;
+                    y = i - 1;
                 }
             }
         }
