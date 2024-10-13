e8008dff3bec1c692b92a3ff7865ca5e8c789d9f
RePrimRose <park1088230@gmail.com>
2024-10-13 18:55:19 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241013/구간 중 최대 합/max-sum-of-subarray.cpp
+++ 241013/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -17,7 +17,7 @@ int main() {
     }
 
     int maxSum = INT_MIN;
-    for(int i = 0; i < n - k; i++) {
+    for(int i = 0; i <= n - k; i++) {
         int sum = 0;
         for(int j = i; j < i + k; j++) {
             sum += arr[j];
