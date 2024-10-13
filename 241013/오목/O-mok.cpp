e40459c5ae5f3d9223e547d31a3a1b5cfc00119d
RePrimRose <park1088230@gmail.com>
2024-10-13 17:13:42 +0900

    Title: 오목, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241013/오목/O-mok.cpp
+++ 241013/오목/O-mok.cpp
@@ -19,32 +19,34 @@ int main() {
             int curr = arr[i][j];
             if(curr == 1 || curr == 2) {
                 int rightStone = 1, underStone = 1, diagonalStone = 1;
-                for(int k = 1; k < 6; k++) {
+                for(int k = 1; k < 5; k++) {
                     if(arr[i][j + k * 1] == curr && j + k * 1 < N) rightStone++;
                     if(arr[i + k * 1][j] == curr && i + k * 1 < N) underStone++;
                     if(arr[i + k * 1][j + k * 1] == curr && j + k * 1 < N && i + k * 1 < N) diagonalStone++;
                 }
                 if(rightStone == 5) {
                     ans = curr;
-                    x = j + 1;
+                    x = j + 3;
                     y = i + 1;
                 }
                 if(underStone == 5) {
                     ans = curr;
                     x = j + 1;
-                    y = i + 1;
+                    y = i + 3;
                 }
                 if(diagonalStone == 5) {
                     ans = curr;
-                    x = j - 1;
-                    y = i - 1;
+                    x = j + 3;
+                    y = i + 3;
                 }
             }
         }
     }
 
     cout << ans << endl;
-    cout << y << " " << x;
+    if(ans != 0) {
+        cout << y << " " << x;
+    }
 
     return 0;
 }
