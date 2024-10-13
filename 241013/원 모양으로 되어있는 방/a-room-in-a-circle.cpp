1f268e4db6f4f0f781e7ab81251d2d804d029a70
RePrimRose <park1088230@gmail.com>
2024-10-13 16:14:52 +0900

    Title: 원 모양으로 되어있는 방, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241013/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[1003];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int minDistance = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        int sumDistance = 0;
+        for(int j = 0; j < n; j++) {
+            sumDistance += (j - i + 5) % 5 * arr[j];
+        }
+        minDistance = min(minDistance, sumDistance);
+    }
+
+    cout << minDistance;
+
+    return 0;
+}
