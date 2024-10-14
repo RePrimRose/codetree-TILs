176850cf2401ac4f1a69a58f99abb5dec5b18c7d
RePrimRose <park1088230@gmail.com>
2024-10-14 20:36:26 +0900

    Title: 아름다운 수열 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241014/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 100
+
+int n, m;
+int A[MAX], aSub[MAX];
+int B[MAX], bSub[MAX];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++)
+        cin >> A[i];
+    
+    for(int i = 0; i < m; i++)
+        cin >> B[i];
+
+    sort(B, B + m);
+
+    int ans = 0;
+    for(int i = 0; i <= n - m; i++) {
+        for(int j = i; j < i + 3; j++) {
+            aSub[j - i] = A[j];
+        }
+        sort(aSub, aSub + 3);
+        int cnt = 0;
+        for(int j = 0; j < m; j++) {
+            if(B[j] == aSub[j]) cnt++;
+        }
+        if(cnt == m) ans++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
