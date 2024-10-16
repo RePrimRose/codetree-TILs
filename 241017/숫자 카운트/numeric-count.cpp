bc060d30b78e404b99673686a412f36399f9aa84
RePrimRose <park1088230@gmail.com>
2024-10-17 04:12:42 +0900

    Title: 숫자 카운트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241017/숫자 카운트/numeric-count.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define MAX 10
+
+int n;
+pair<int, pair<int, int>> arr[MAX];
+
+int makeThreeDigitNumber(int i, int j, int k) {
+    return i * 100 + j * 10 + k;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i].first >> arr[i].second.first >> arr[i].second.second;
+    
+    int ans = 0;
+    for(int i = 1; i <= 9; i++)
+        for(int j = 1; j <= 9; j++)
+            for(int k = 1; k <= 9; k++) {
+                bool isPossible = true;
+                for(int l = 0; l < n; l++) {
+                    string numberOne = to_string(arr[l].first);
+                    string numberTwo = to_string(makeThreeDigitNumber(i, j, k));
+                    int firstCnt = 0, secondCnt = 0;
+                    for(int m = 0; m < 3; m++) {
+                        if(numberOne[m] == numberTwo[m]) firstCnt++;
+                        else if(numberTwo.find(numberOne[m]) != string::npos) secondCnt++;
+                    }
+                    if(firstCnt != arr[l].second.first || secondCnt != arr[l].second.second) {
+                        isPossible = false;
+                        break;
+                    }
+                }
+                if(isPossible) ans++;
+            }
+    
+    cout << ans;
+
+    return 0;
+}
