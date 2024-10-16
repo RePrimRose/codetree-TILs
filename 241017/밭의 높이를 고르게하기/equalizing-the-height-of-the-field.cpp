1bff7c7ee401c6ca7bf53781ecf70b37a2445459
RePrimRose <park1088230@gmail.com>
2024-10-17 03:08:52 +0900

    Title: 밭의 높이를 고르게하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241017/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 241017/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -15,7 +15,7 @@ int main() {
         cin >> arr[i];
     
     int minCost = INT_MAX;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i <= n - t; i++) {
         int sumCost = 0;
         for(int j = i; j < i + t; j++) {
             sumCost += abs(h - arr[j]);
