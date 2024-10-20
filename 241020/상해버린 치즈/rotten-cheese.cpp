217a56e78cd8db00d887105854a656fcdb90da2c
RePrimRose <park1088230@gmail.com>
2024-10-20 18:51:45 +0900

    Title: 상해버린 치즈, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241020/상해버린 치즈/rotten-cheese.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 51
+#define MAX_RECORD 1000
+
+int n, m, d, s;
+int eaten_count[MAX];
+pair<int, pair<int, int>> eaten_record[MAX];
+pair<int, int> sick_record[MAX_RECORD];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m >> d >> s;
+
+    for(int i = 0; i < d; i++)
+        cin >> eaten_record[i].first >> eaten_record[i].second.first >> eaten_record[i].second.second;
+
+    for(int i = 0; i < s; i++)
+        cin >> sick_record[i].first >> sick_record[i].second;
+
+    for(int i = 0; i < s; i++) {
+        int sick_person = sick_record[i].first, sick_time = sick_record[i].second;
+        for(int j = 0; j < d; j++) {
+            int eaten_person = eaten_record[j].first; 
+            int eaten_cheese = eaten_record[j].second.first;
+            int eaten_time = eaten_record[j].second.second;
+            if(sick_person == eaten_person && eaten_time < sick_time) eaten_count[eaten_cheese]++;
+        }
+    }
+
+    int ans = 0;
+    for(int i = 1; i <= m; i++) {
+        if(eaten_count[i] == s) {
+            int cnt = 0;
+            for(int j = 0; j < d; j++) {
+                int eaten_cheese = eaten_record[j].second.first;
+                if(eaten_cheese == i) cnt++;
+            }
+            ans = max(ans, cnt);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
