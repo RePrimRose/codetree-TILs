d506af8e8b74a455950825d04ff870fd85b7fba7
RePrimRose <park1088230@gmail.com>
2024-10-20 18:15:08 +0900

    Title: 스승의 은혜 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241020/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 1000
+
+int n, b;
+int p[MAX];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> b;
+
+    for(int i = 0; i < n; i++)
+        cin >> p[i];
+    
+    sort(p, p + n);
+
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        int b_copy = b, cnt = 0;
+        for(int j = 0; j < n; j++) {
+            if(i == j) b_copy -= p[j] / 2;
+            else b_copy -= p[j];
+            if(b_copy < 1) break;
+            cnt++;
+        }
+        ans = max(ans, cnt);
+    }
+
+    cout << ans;
+
+    return 0;
+}
