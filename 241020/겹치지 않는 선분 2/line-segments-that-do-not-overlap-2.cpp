2039e08a13301071e6cf860804751355798a6fe7
RePrimRose <park1088230@gmail.com>
2024-10-20 16:10:41 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241020/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 241020/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -14,10 +14,13 @@ int main() {
     for(int i = 0; i < n; i++)
         cin >> x1[i] >> x2[i];
 
-    for(int i = 0; i < n; i++)
-        for(int j = i + 1; j < n; j++)
-            if((x1[i] - x1[j]) * (x2[i] - x2[j]) < 0)
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if((x1[i] - x1[j]) * (x2[i] - x2[j]) < 0) {
                 isOverlapped[i] = true; isOverlapped[j] = true;
+            }
+        }
+    }
 
     int ans = 0;
     for(int i = 0; i < n; i++) {
