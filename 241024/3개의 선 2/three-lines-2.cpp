d89e929bc713e2d86777a4d5ee071d447575afec
RePrimRose <park1088230@gmail.com>
2024-10-24 03:30:04 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241024/3개의 선 2/three-lines-2.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define XY_MAX 20
+
+int n;
+int x[XY_MAX], y[XY_MAX];
+
+bool x3() {
+    for(int i = 0; i <= 10; i++) {
+        for(int j = i + 1; j <= 10; j++) {
+            bool isPossible = true;
+            for(int k = j + 1; k <= 10; k++) {
+                if(x[i] != i && x[i] != j && x[i] != k) {
+                    isPossible = false;
+                    break;
+                }
+            }
+            if(isPossible) return true;
+        }
+    }
+
+    return false;
+}
+
+bool y3() {
+    for(int i = 0; i <= 10; i++) {
+        for(int j = i + 1; j <= 10; j++) {
+            bool isPossible = true;
+            for(int k = j + 1; k <= 10; k++) {
+                if(y[i] != i && y[i] != j && y[i] != k) {
+                    isPossible = false;
+                    break;
+                }
+            }
+            if(isPossible) return true;
+        }
+    }
+
+    return false;
+}
+
+bool x2y1() {
+    for(int i = 0; i <= 10; i++) {
+        for(int j = i + 1; j <= 10; j++) {
+            bool isPossible = true;
+            for(int k = 0; k <= 10; k++) {
+                if(x[i] != i && x[i] != j && y[i] != k) {
+                    isPossible = false;
+                    break;
+                }
+            }
+            if(isPossible) return true;
+        }
+    }
+
+    return false;
+}
+
+bool x1y2() {
+    for(int i = 0; i <= 10; i++) {
+        for(int j = i + 1; j <= 10; j++) {
+            bool isPossible = true;
+            for(int k = 0; k <= 10; k++) {
+                if(y[i] != i && y[i] != j && x[i] != k) {
+                    isPossible = false;
+                    break;
+                }
+            }
+            if(isPossible) return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    if(x3() || y3() || x2y1() || x1y2()) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
