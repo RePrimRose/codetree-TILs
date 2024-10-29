9f5cbf42b86eb713caa283d0afde48e70e116505
RePrimRose <park1088230@gmail.com>
2024-10-30 02:09:21 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241030/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 100
+
+int n, k;
+int bomb[MAX];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++)
+        cin >> bomb[i];
+
+    int ans = 0;
+    int max_cnt = 0;
+    for(int i = 0; i < n; i++) {
+        int cnt = 0;
+
+        for(int j = i + 1; j <= i + k + 1; j++) {
+            if(j == n) break;
+            if(bomb[i] == bomb[j]) cnt++;
+        }
+
+        if(cnt >= max_cnt && cnt > 0) {
+            max_cnt = cnt;
+            ans = max(ans, bomb[i]);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
