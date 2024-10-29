85d9504a51f758498173e01e548a413bfae3b4eb
RePrimRose <park1088230@gmail.com>
2024-10-30 01:56:23 +0900

    Title: 초기 수열 복원하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241030/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#define MAX 1000
+
+int n;
+int arr[MAX];
+
+bool isPossible(int a) {
+    bool isUsed[MAX] = {};
+    isUsed[a] = true;
+
+    int curr_num = a;
+    for(int i = 0; i < n - 1; i++) {
+        curr_num = abs(arr[i] - curr_num);
+        if(isUsed[curr_num]) return false;
+        else isUsed[curr_num] = true;
+    }
+
+    return true;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    for(int i = 0; i < n - 1; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 1; i < arr[0]; i++) {
+        if(isPossible(i)) {
+            int curr_num = i;
+            cout << curr_num << " ";
+            for(int i = 0; i < n - 1; i++) {
+                curr_num = abs(arr[i] - curr_num);
+                cout << curr_num << " ";
+            }
+            break;
+        }
+    }
+
+    return 0;
+}
