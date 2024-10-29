9070e79b8a66597385e0afaeb3c18c067c0ad942
RePrimRose <park1088230@gmail.com>
2024-10-30 01:01:54 +0900

    Title: 훌륭한 점프, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241030/훌륭한 점프/great-jump.cpp
+++ 241030/훌륭한 점프/great-jump.cpp
@@ -3,7 +3,7 @@
 #include <climits>
 using namespace std;
 
-#define MAX 100
+#define MAX 101
 
 int n, k;
 int rock[MAX];
@@ -12,13 +12,13 @@ bool IsPossible(int max_val) {
     int available_rocks[MAX] = {};
     int cnt = 0;
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         if(rock[i] <= max_val) {
             available_rocks[cnt++] = i;
         }
     }
 
-    if(available_rocks[0] != 0) return false;
+    if(available_rocks[0] != 1) return false;
 
     for(int i = 1; i < cnt; i++) {
         int dist = available_rocks[i] - available_rocks[i - 1];
@@ -32,7 +32,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n >> k;
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         cin >> rock[i];
     }
 
