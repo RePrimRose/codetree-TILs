7387ce51783a7770b7f71847d3c9ddf6dc169d0e
RePrimRose <park1088230@gmail.com>
2024-10-30 01:34:38 +0900

    Title: A, B, C, D 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
+++ 241030/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 #define MAX 15
 
-int arr[MAX];
+vector<int> arr;
+
+vector<int> make_arr(int a, int b, int c, int d) {
+    vector<int> new_arr;
+
+    new_arr.push_back(a + b + c + d);
+    new_arr.push_back(a + b + c);
+    new_arr.push_back(a + b + d);
+    new_arr.push_back(a + c + d);
+    new_arr.push_back(b + c + d);
+    new_arr.push_back(a + b);
+    new_arr.push_back(a + d);
+    new_arr.push_back(b + d);
+    new_arr.push_back(a + c);
+    new_arr.push_back(c + d);
+    new_arr.push_back(b + c);
+    new_arr.push_back(a);
+    new_arr.push_back(b);
+    new_arr.push_back(c);
+    new_arr.push_back(d);
+    sort(new_arr.begin(), new_arr.end());
+
+    return new_arr;
+}
 
 int main() {
     // 여기에 코드를 작성해주세요.
     for(int i = 0; i < MAX; i++) {
-        cin >> arr[i];
+        int num;
+        cin >> num;
+        arr.push_back(num);
     }
 
-    sort(arr, arr + MAX);
+    sort(arr.begin(), arr.end());
 
     bool isFind = false;
     for(int i = 0; i < MAX; i++) {
         for(int j = i + 1; j < MAX; j++) {
             for(int k = j + 1; k < MAX; k++) {
                 for(int l = k + 1; l < MAX; l++) {
-                    if(arr[i] + arr[j] + arr[k] + arr[l] == arr[MAX - 1]) {
+                    vector<int> temp = make_arr(arr[i], arr[j], arr[k], arr[l]);
+                    if(arr == temp) {
                         cout << arr[i] << " " << arr[j] << " " << arr[k] << " " << arr[l];
                         isFind = true;
                     }
