a979283b63933e969dc74e58f26132973bc2c3f5
RePrimRose <park1088230@gmail.com>
2024-10-30 01:38:39 +0900

    Title: 독서실의 거리두기 4, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241030/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <string>
+using namespace std;
+
+int n;
+string str;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> str;
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (str[i] == '1') continue;
+
+        string seat = str;
+        seat[i] = '1';
+
+        for (int j = i + 1; j < n; j++) {
+            if (str[j] == '1') continue;
+
+            string seat = str;
+            seat[j] = '1';
+
+            int distance = 0;
+            int min_distance = INT_MAX;
+            int seat_count = 0;
+
+            for (int k = 0; k < n; k++) {
+                seat_count += seat[k] - '0';
+
+                if (seat_count == 1) distance++;
+                if (seat_count == 2) {
+                    min_distance = min(min_distance, distance);
+                    distance = 1;
+                    seat_count = 1;
+                }
+            }
+            ans = max(ans, min_distance);
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
