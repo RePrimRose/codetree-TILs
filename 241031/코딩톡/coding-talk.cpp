b6e8a939b808cb6edc82ecfd1d83e351b549d8ec
RePrimRose <park1088230@gmail.com>
2024-10-31 01:37:41 +0900

    Title: 코딩톡, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241031/코딩톡/coding-talk.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+int n, m, p;
+int u[MAX];
+char c[MAX];
+bool people[26];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m >> p;
+
+    for(int i = 0; i < m; i++) {
+        cin >> c[i] >> u[i];
+    }
+
+    int target_u = u[p - 1];
+    for(int i = p - 1; i >= 0; i--) {
+        if(u[i] == target_u) people[c[i] - 'A'] = true;
+    }
+
+    for(int i = p - 1; i < n; i++) {
+        people[c[i] - 'A'] = true;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(people[i] == false) cout << (char)(i + 'A') << " ";
+    }
+
+    return 0;
+}
