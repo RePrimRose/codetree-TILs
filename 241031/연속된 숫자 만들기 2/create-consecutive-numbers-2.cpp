d57f2b900d5fbe71d1908472b7a78863f4b659ea
RePrimRose <park1088230@gmail.com>
2024-10-31 04:02:03 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241031/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 241031/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <algorithm>
 #include <cstdlib>
 using namespace std;
 
 #define MAX 3
 
 int people[MAX];
-bool connect[MAX];
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -13,23 +13,12 @@ int main() {
         cin >> people[i];
     }
 
-    for(int i = 0; i < MAX; i++) {
-        for(int j = i + 1; j < MAX; j++) {
-            if(abs(people[i] - people[j]) == 1) {
-                connect[i] = true;
-                connect[j] = true;
-            }
-        }
-    }
+    sort(people, people + MAX);
 
-    int cnt = 0;
-    for(int i = 0; i < MAX; i++) {
-        if(connect[i]) cnt++;
-    }
+    if(people[2] - people[1] == 1 && people[1] - people[0] == 1) cout << 0;
+    else if(people[2] - people[1] > 2 && people[1] - people[0] > 1) cout << 2;
+    else cout << 1;
 
-    if(cnt == 3) cout << 0;
-    else if(cnt == 2) cout << 1;
-    else cout << 2;
 
     return 0;
 }
