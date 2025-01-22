2c90e38e87ae922f5afa50a6af69a42288005b04
RePrimRose <park1088230@gmail.com>
2025-01-22 09:55:59 +0900

    Title: 황금비율 토스트, Time: 16ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 250122/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+list<char> l;
+int n, m;
+string s;
+
+int main() {
+    cin >> n >> m;
+    cin >> s;
+
+    for (int i = 0; i < n; i++) {
+        l.push_back(s[i]);
+    }
+
+    list<char>::iterator it;
+    it = l.end();
+
+    for (int i = 0; i < m; i++) {
+        char command;
+        cin >> command;
+        if (command == 'P') {
+            char c;
+            cin >> c;
+            l.insert(it, c);
+        } else if (command == 'L') {
+            if(it == l.begin()) continue;
+            it--;
+        } else if (command == 'R') {
+            if(it == l.end()) continue;
+            it++;
+        } else {
+            if(it == l.end()) continue;
+            l.erase(it);
+        }
+    }
+
+    for (it = l.begin(); it != l.end(); it++) {
+        cout << *it;
+    }
+
+    return 0;
+}
