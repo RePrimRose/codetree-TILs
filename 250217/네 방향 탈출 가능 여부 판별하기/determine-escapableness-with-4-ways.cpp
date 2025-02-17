57c8d9e757b11a3b0a6f98994ab5021ee1f30037
RePrimRose <park1088230@gmail.com>
2025-02-17 10:30:08 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 554ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 250217/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int a[100][100];
+bool visited[100][100];
+queue<pair<int, int>> q;
+
+bool in_range(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < m;
+}
+
+bool can_go(int x, int y) {
+    if(!in_range(x, y)) return false;
+    if(visited[x][y] || a[x][y] == 0) return false;
+
+    return true;
+}
+
+void bfs() {
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+
+    while(!q.empty()) {
+        pair<int, int> curr_pos = q.front();
+        q.pop();
+
+        int x = curr_pos.first;
+        int y = curr_pos.second;
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(can_go(nx, ny)) {
+                q.push({nx, ny});
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    // Write your code here!
+    q.push({0, 0});
+    bfs();
+
+    if(visited[n - 1][m - 1] == true) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
