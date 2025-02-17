a5ccf18fc1d157a56783119b47cd46bfda9b06c6
RePrimRose <park1088230@gmail.com>
2025-02-17 23:02:06 +0900

    Title: 1, 2, 5 더하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250217/1, 2, 5 더하기/1-2-5-plus.cpp
+++ 250217/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -10,17 +10,17 @@ int main() {
 
     // Write your code here!
     dp[0] = 1;
-    dp[1] = 1;
 
-    for (int i = 2; i <= 4; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+    for (int i = 1; i <= n; i++) {
+        if (i - 1 >= 0)
+            dp[i] = (dp[i] + dp[i - 1]) % 10007;
+        if (i - 2 >= 0)
+            dp[i] = (dp[i] + dp[i - 2]) % 10007;
+        if (i - 5 >= 0)
+            dp[i] = (dp[i] + dp[i - 5]) % 10007;
     }
 
-    for (int i = 5; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 5];
-    }
-
-    cout << dp[n] % 10007;
+    cout << dp[n];
 
     return 0;
 }
